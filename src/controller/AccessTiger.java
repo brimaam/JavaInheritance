d9c22ffa5ef21b5101c40d4ac03bd093420fd6f2
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-07-27 10:23:24 +0800

    added a few comments

--- src/controller/AccessTiger.java
+++ src/controller/AccessTiger.java
@@ -4,13 +4,14 @@ import model.Tiger;
 
 public class AccessTiger {
     public static void main(String[] args) {
+        //created tiger instance
         Tiger tiger = new Tiger(15, 220.43,"Shere Khan");
 
         System.out.println("Tiger Name: " + tiger.getName());
         System.out.println("Tiger Weight: " + tiger.getWeight() + " kg");
         System.out.println("Tiger Age: " + tiger.getAge());
-        System.out.println("What the tiger does?");
-        tiger.meowing();
+        System.out.println("\nWhat the tiger does?");
+        tiger.meowing(); //tiger produces meow
         tiger.running();
         tiger.eating();
     }
